61c5520d895347cab603ae5d54882a4c93056b3d
seema_umesh <dseemasingh@gmail.com>
2019-01-28 10:33:35 +1100

    result-test

---
+++ ClientApp/src/app/result/result.component.spec.ts
@@ -0,0 +1,27 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ResultComponent } from './result.component';
+
+describe('ResultComponent', () => {
+  let component: ResultComponent;
+  let fixture: ComponentFixture<ResultComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ResultComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ResultComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should display a title', async(() => {
+    const titleText = fixture.nativeElement.querySelector('h1').textContent;
+    expect(titleText).toEqual('Result');
+  }));
+
+});
